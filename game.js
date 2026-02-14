aeeb0227971e0317a50c5500a57f4e9cfaf2d13d
Juan Rios <jsrioss1@academia.usbbog.edu.co>
2026-02-13 21:54:04 -0500

    game finished

--- game.js
+++ game.js
@@ -1,21 +1,18 @@
-console.log("Adivina el número del 1 al 10")
+console.log("Adivina el número del 1 al 10");
 
-const RandomNum = Math.floor(Math.random()*10);
-console.log(numeroRandom);
+let randomNum = Math.floor(Math.random() * 10) + 1;
 
-While(){
-    let numUser = prompt('Adivina el numero');
-    let rango = RandomNum/numUser;
-    if(rango == RandomNum){
-        console.log('!GANASTE¡');
+while(true){
+    let numUser = Number(prompt("Adivina el número"));
+
+    if(numUser === randomNum){
+        console.log("¡GANASTE!");
         break;
     }
-    else if(rango >= 0.1 && rango <= 1.25 ){
-        console.log('Caliente');
-        continue;
-    }  
-    else{
-        console.log('Frio');
-        continue;
+    else if(numUser > randomNum){
+        console.log("Frio");
     }
-};
+    else{
+        console.log("Caliente");
+    }
+}
